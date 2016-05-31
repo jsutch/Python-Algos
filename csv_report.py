4117fc65520c79c956abe1ebb5d8c40d6f117694
Jeff Sutch <jeff.sutch@acm.org>
2016-05-30 20:39:23 -0700

    Working CSV reader

--- csv_report.py
+++ csv_report.py
@@ -4,25 +4,24 @@ import csv
 
 users = {}
 
-with open('mycsv.csv', 'rU') as csv_file:
-	reader = csv.reader(csv_file)
-	labels = reader.next()
-	booklist = []
+with open('mycsv1.csv', 'rU') as csv_file:
+	reader = csv.reader(csv_file) # csv.reader is a function in the csv module
+	labels = reader.next() # reader.next() is a function in the csv.reader function
+	booklist = [] # make a new, empty list called booklist
 
 
 	for row in reader:
-		# create  2-tuple list from two lists - labels and row (proto dictionary)
-		entry = zip(labels, row)
+		# print row # debugging
+		entry = zip(labels, row) # create  2-tuple list from two lists - labels and row (proto dictionary)
 		#print entry # prints full entry - debugging
-		#update the list
-		booklist.append(entry)
-		#create a dictionary called addressbook from the  2-tuple list form of entry
-		addressbook = dict(entry)
-		# extract the values from addressbook using the list[name]
-		# this will now print First Name and Last Name
-		print '{a[first_name]} {a[last_name]}'.format(a=addressbook)
+		booklist.append(entry) #update the list 'booklist'
+		# print booklist # print booklist - debugging
+		addressbook = dict(entry) #create a dictionary called addressbook from the  2-tuple list form of entry
+		print '{a[first_name]} {a[last_name]}'.format(a=addressbook) # this will now print First Name and Last Name columns
+		# print labels # debugging
+		for label in labels:
+			#print label #debugging
+			#print addressbook[label] #debugging
+			print "{}: {} \n".format(label, addressbook[label])
+		print "------------------------------"
 
-	for label in labels:
-		# pulls in the values from label and addressbook in the format of "label: value" with newline and ------ between records
-		print "{}: {} \n".format(label, addressbook[label])
-	print "---------------------------"	
