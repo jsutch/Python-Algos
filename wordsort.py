8f54cdb8f06e4299183baef529e51bf1e1b39767
Jeff Sutch <jeff.sutch@acm.org>
2020-04-29 16:20:31 -0700

    updated wordsort functions

--- wordsort.py
+++ wordsort.py
@@ -1,9 +1,9 @@
-def wordsort(word):
+def lettersort(word):
     """
     Sort the letters of a string alphabetically by ascii comparison.
     Return as a string.
     """
-    strarr = list(str(word))
+    strarr = list(str((word))
     for i in range(len(strarr) - 1):
         for j in range(len(strarr) - i - 1):
             if strarr[j] > strarr[j + 1]:
@@ -13,7 +13,7 @@ def wordsort(word):
 
 
 
-def reversewordsort(word):
+def reverselettersort(word):
     """
     Sort the letters of a string alphabetically by ascii comparison.
     Return as a string.
@@ -28,4 +28,22 @@ def reversewordsort(word):
     return newstr
 
 
-wordsort('supercalifragilisticexplialidocious')
+lettersort('supercalifragilisticexplialidocious')
+
+def sortwords(phrase):
+    words = phrase.split()
+    for loop in range(len(words) -1, -1, -1):
+        for x in range(loop):
+            if words[x][0] > words[x + 1][0]:
+                words[x],  words[x+1] =  words[x+1],  words[x]
+    return words
+words = phrase.split()
+sortwords(phrase)
+def sortwordsreverse(phrase):
+    words = phrase.split()
+    for loop in range(len(words) -1, -1, -1):
+        for x in range(loop):
+            if words[x][0] > words[x + 1][0]:
+                words[x],  words[x+1] =  words[x+1],  words[x]
+    return words[::-1]
+
