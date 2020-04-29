c3b3ddc6411679e16eed162c3a3ed99983a1f644
Jeff Sutch <jeff.sutch@acm.org>
2020-04-29 16:59:55 -0700

    fixed palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,25 +1,10 @@
-#!/usr/local/bin/python
+def ispal(word):
+    if " " in word:
+        word = "".join(word.split())
+    for i in range(0, len(word)//2):
+        if word[i] != word[len(word) -i -1]:
+            return False
+    return True
 
-#broken - doesn't recognize some words. Doesn't handle multi-word palindromes
-def palindrome(word):
-	MISS = 0
-	wordlist = word.split()
-	#print word.split()
-	string = ''.join(wordlist)
-	#print string
-	for x in range(0,len(string)):
-		#print word[x], word[len(word) -x - 1]
-		if string[x] == string[len(string) -x - 1]:
-			#print word[x], " is ", word[len(word) -x - 1]
-			pass
-		else: 
-			#print word[x], " is NOT", word[len(word) -x - 1]
-			MISS += 1
-	if MISS != 0:
-		return "FALSE"
-	else: 
-		return "TRUE"
-
-
-#print palindrome('amanaplanacanalpanama')
+print palindrome('amanaplanacanalpanama')
 print(palindrome('a man a plan a canal panama'))
