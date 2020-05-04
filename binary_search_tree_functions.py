a7924af58e5859b97a82b6b9650716e485a2a8ab
Jeff Sutch <jeff.sutch@acm.org>
2020-05-04 13:22:53 -0700

    added balancing and deletes to binary search tree functions

---
+++ binary_search_tree_functions.py
@@ -0,0 +1,264 @@
+class BSTNode(object):
+    def __init__(self, key):
+        self.val = key
+        self.left = self.right =  None
+
+
+# BST Methods
+def insert(root, node):
+    """
+    insert the given key into the BST
+    """
+    # case where root is empty
+    if root is None:
+        root = node
+    else:
+        # case where tree exists and value goes right
+        if root.val < node.val:
+            if root.right is None:
+                root.right = node
+            else:
+                insert(root.right, node)
+        else:
+            if root.left is None:
+                root.left = node
+            else:
+                insert(root.left, node)
+def search(root, key):
+    """
+    Does key exist in the BST?
+    """
+    if root is None:
+        return False
+    elif root.val == key:
+        return root.val
+    elif root.val > key:
+        return search(root.left, key)
+    return search(root.right, key)
+
+
+def minvalue(node): 
+    """
+    return the smallest valued node. needed for delete
+    """
+    cursor = node 
+    # loop down to find the leftmost leaf 
+    while(cursor.left is not None): 
+        cursor = cursor.left  
+    return cursor  
+
+def minvalueval(node): 
+    """
+    return the smallest valued node's value for testing
+    """
+    cursor = node 
+    # loop down to find the leftmost leaf 
+    while(cursor.left is not None): 
+        cursor = cursor.left  
+    return cursor.val
+
+def randomval(root):
+    """
+    return a random value from from the bst.
+    utility to test delete. Currently borked.
+    """
+    myarr = []
+    cursor = root
+    while(cursor.left is not None):
+        myarr.append(cursor.val)
+        cursor = cursor.left
+    myarr.append(cursor.val)
+    while(cursor.right is not None):
+        myarr.append(cursor.val)
+        cursor = cursor.right
+    return random.choice(myarr)
+    
+def delete(root, key):
+    """
+    returns a BST without the deleted value
+    """
+    if root is None:
+        return root
+    # Is key in left or right?
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif (key > root.val):
+        root.right = delete(root.right, key)
+    # Is root the key?
+    else:
+    # Case with no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        # with two children get the inOrder successor
+        # smallest on right subtree
+        temp = minvalue(root.right)
+        root.val = temp.val
+        root.right = delete(root.right, temp.val)
+    #
+    return root
+
+
+
+# Traversals
+# Depth First traversals
+def inorder(root):
+    """
+    DFT returns nodes in non-decreasing order
+    print left -> root -> right
+    """
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+def preorder(root):
+    """
+    DFT used to create a copy of the tree
+    print root -> left -> right
+    """
+    if root:
+        print(root.val)
+        preorder(root.left)
+        preorder(root.right)
+
+def postorder(root):
+    """
+    DFT - used to delete a tree
+    print left -> right -> root
+    """
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val)
+
+
+# Breadth first Traversals
+def height(node):
+    """
+    Find height of node. Needed for levelorder traversal
+    """
+    if node is None:
+        return 0
+    else:
+        lh = height(node.left)
+        rh = height(node.right)
+        #
+        if lh > rh:
+            return lh + 1
+        else:
+            return rh + 1
+
+def givenlevel(root, level):
+    """
+    find the givenlevel of the level. Needed for levelorder traversal
+    """
+    if root is None:
+        return
+    if level == 1:
+        print(root.val)
+    elif level > 1:
+        givenlevel(root.left, level -1)
+        givenlevel(root.right, level -1)
+
+def levelorder(root):
+    """
+    a BFT that prints all nodes per level
+    """
+    h = height(root)
+    for i in range(1, h + 1):
+        givenlevel(root, i)
+
+
+def printlevel(root):
+    [(print("=== Level", x, "==="), givenlevel(root, x)) for x in range(1,height(root))]
+
+
+# Balancing a tree
+# is a tree balanced?
+def isbalanced(root):
+    if root is None:
+        return True
+    return is_balanced(root.right) and is_balanced(root.left) and abs(height(root.left) - height(root.right)) <= 1
+
+def store(root, nodearr):
+    """
+    recreate the bst as an inorder array.
+    needed by balance
+    """
+    if not root:
+        return
+    store(root.left)
+    nodearr.append(root.val)
+    store(root.right)
+
+
+def build(nodearr):
+    """
+    rebuild the bst from the inorder array
+    needed by balance
+    """
+    if not noderarr:
+        return None
+    mid = len(nodearr) // 2
+    node = BSTNode(nodearr[mid])
+    node.left = newBST(nodearr[:mid])
+    node.right = newBST(nodearr[mid + 1:])
+    return node
+
+def balance(root):
+    """
+    - get inorder traversal of existing BST as an array
+    - create a new BST object sorted by the midpoint
+    - return the object
+    """
+    nodes = []
+    store(root, nodes)
+    return newBST(nodes)
+
+
+
+
+
+# Helper code
+# create and  populate a tree
+meh = BSTNode(500)
+[insert(meh, BSTNode(random.randint(1,1024))) for x in range(20)]
+
+
+# test harness
+
+foo = BSTNode(500)
+[insert(foo, BSTNode(random.randint(1,1024))) for x in range(20)]
+print("printlevels")
+printlevel(foo)
+print("DFS traversing")
+print("inorder traverse")
+inorder(foo)
+print("preorder traverse")
+preorder(foo)
+print("postorder traverse")
+postorder(foo)
+print("BFS traversing")
+levelorder(foo)
+print("is balanced?")
+isbalanced(foo)
+foo = balance(foo)
+isbalanced(foo)
+print("inorder - balanced")
+inorder(foo)
+printlevel(foo)
+
+# Deletes harness - not working yet
+# grab random value from the tree
+# randomvalue = randval(foo)
+# foo = delete(foo, randomvalue)
+# search(foo, randomvalue)
+# printlevel(foo)
+# isbalanced(foo)
+
