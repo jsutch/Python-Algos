3a6037f2fa518c9d68a04253963e8c29c091fc54
Jeff Sutch <jeff.sutch@acm.org>
2020-05-04 15:51:31 -0700

    added tests for balance parens

--- balance_parens.py
+++ balance_parens.py
@@ -1,3 +1,14 @@
+test1='(h[e{lo}!]<~)()()()(' #unbalanced - 2 leftovers (, <
+# test1='(h[e{lo}!]<~)(' #unbalanced - 1 leftover (
+test2='({)}' #unbalanced - out of order closure - 2 leftovers ({
+test3='(]' #unbalanced - 1 leftover (
+test4=')' #unbalanced, empty stack test
+test5='()' #balanced
+test6='({<>})' #balanced
+test7='(h[e{lo}!]~)' #balanced
+test8='[](){}<>' #balanced, no leftovers
+
+
 def balanced(string):
     stack=[]
     opens, closes = "<({[", ">)}]"
@@ -16,3 +27,13 @@ def balanced(string):
                 if (last, c) not in match:
                     return 0, "Unbalanced - stack may have leftovers", stack
     return 1, "Balanced - stack should be empty", stack
+
+
+print("Test 1", balanced(test1))
+print("Test 2", balanced(test2))
+print("Test 3", balanced(test3))
+print("Test 4", balanced(test4))
+print("Test 5", balanced(test5))
+print("Test 6", balanced(test6))
+print("Test 7", balanced(test7))
+print("Test 8", balanced(test8))
