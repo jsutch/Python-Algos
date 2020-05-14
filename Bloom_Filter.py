81aa3edd6c2e9622ef0ac981f66dc409bb9e88d3
Jeff Sutch <jeff.sutch@acm.org>
2020-05-14 12:06:13 -0700

    added bloom filter example

---
+++ Bloom_Filter.py
@@ -0,0 +1,31 @@
+import mmh3
+from bitarray import bitarray
+
+class BloomFilter(set):
+
+    def __init__(self, size, hash_count):
+        super(BloomFilter, self).__init__()
+        self.bit_array = bitarray(size)
+        self.bit_array.setall(0)
+        self.size = size
+        self.hash_count = hash_count
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return iter(self.bit_array)
+
+    def add(self, item):
+        for ii in range(self.hash_count):
+            index = mmh3.hash(item, ii) % self.size
+            self.bit_array[index] = 1
+        return self
+
+    def __contains__(self, item):
+        out = True
+        for ii in range(self.hash_count):
+            index = mmh3.hash(item, ii) % self.size
+            if self.bit_array[index] == 0:
+                out = False
+        return out
