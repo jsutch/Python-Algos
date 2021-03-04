e262b19c554fde3d9027b986c66604c0a8ed3dcd
Jeff Sutch <jeff.sutch@acm.org>
2021-03-04 12:58:00 -0800

    added function for swapping values in array

--- swapping_values.py
+++ swapping_values.py
@@ -13,3 +13,26 @@ my_array[0] = my_array[last]
 my_array[last] = temp
 
 print(my_array)
+
+# as a function with some bounds checking
+In [155]: myarr
+Out[155]: [53, 32, 38, 61, 57, 44, 90, 81, 26, 15, 69]
+
+In [157]: def swap_values(x, y, arr):
+     ...:     if (x < len(arr)) and (y < len(arr)) and (x !=y):
+     ...:         arr[x],arr[y] = arr[y],arr[x]
+     ...:         return arr
+     ...:     else:
+     ...:         return 'Indexes must be smaller than the array and not the same'
+     ...:
+     ...:
+     ...:
+
+In [158]: swap_values(22,6,myarr)
+Out[158]: 'Indexes must be smaller than the array and not the same'
+
+In [159]: swap_values(6,6,myarr)
+Out[159]: 'Indexes must be smaller than the array and not the same'
+
+In [160]: swap_values(2,8,myarr)
+Out[160]: [53, 32, 26, 90, 57, 44, 61, 81, 38, 15, 69]
