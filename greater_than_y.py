d9b09105d068f0a0b6b0c2d40f54fbbcdd4cd2c6
Jeff Sutch <jeff.sutch@acm.org>
2021-03-04 12:50:15 -0800

    updated 'is x greater than any values in arr?'

--- greater_than_y.py
+++ greater_than_y.py
@@ -12,3 +12,47 @@ while loop < len(my_array):
 	loop += 1
 
 print("from the list", my_array, "only" , output ,"are greater than", y)
+
+# slightly cleaner:
+In [142]: n=66
+In [143]: output = []
+     ...: for x in range(0,len(myarr)):
+     ...:     if myarr[x] < n:
+     ...:         output.append(myarr[x])
+     ...:
+
+In [148]: print(f'only {output} are greater than {n} in {myarr}')
+only [60, 25, 61, 25, 14, 7, 60] are greater than 66 in [60, 25, 90, 92, 92, 61, 25, 14, 7, 90, 60]
+
+
+# as function return indexes as well -python3
+In [136]: def greater_than(arr,n):
+     ...:     """
+     ...:     find if a given integer is larger than other integers in an array.
+     ...:     return indexes and the numbers if true, False if not
+     ...:     """
+     ...:     biggers = {}
+     ...:     for i in range(0,len(arr)):
+     ...:         if n > arr[i]:
+     ...:             biggers[i] = arr[i]
+     ...:     if biggers:
+     ...:         return biggers
+     ...:     else:
+     ...:         False
+     ...:
+     ...:
+
+In [137]: greater_than(myarr,66)
+Out[137]: {0: 60, 1: 25, 6: 61, 7: 25, 8: 14, 9: 7, 11: 60}
+
+In [138]: myarr
+Out[138]: [60, 25, 90, 92, 666666, 92, 61, 25, 14, 7, 90, 60]
+
+In [139]: myarr.pop(4)
+Out[139]: 666666
+
+In [140]: myarr
+Out[140]: [60, 25, 90, 92, 92, 61, 25, 14, 7, 90, 60]
+
+In [141]: greater_than(myarr,66)
+Out[141]: {0: 60, 1: 25, 5: 61, 6: 25, 7: 14, 8: 7, 10: 60}
