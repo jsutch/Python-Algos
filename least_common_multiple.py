4eb9c5721c8d1920b30286664706808bb0168be9
Jeff Sutch <jeff@collettpark.com>
2022-01-25 09:44:15 -0800

    added range example for least common multiple

--- least_common_multiple.py
+++ least_common_multiple.py
@@ -31,4 +31,17 @@ In [71]: print(lcm(3,5))
 
 In [72]: print(lcm(6,9))
 18
+
+In [85]: for x,y in zip(range(1,101),range(101,1,-1)):
+    ...:     print(f'{x} and {y} have a least common multiple of {lcd(x,y,100)} up to a limit of 100')
+    ...:
+    ...:
+1 and 101 have a least common multiple of None up to a limit of 100
+2 and 100 have a least common multiple of 100 up to a limit of 100
+3 and 99 have a least common multiple of 99 up to a limit of 100
+4 and 98 have a least common multiple of 196 up to a limit of 100
+5 and 97 have a least common multiple of 485 up to a limit of 100
+6 and 96 have a least common multiple of 96 up to a limit of 100
+7 and 95 have a least common multiple of 665 up to a limit of 100
+<...>
 """
