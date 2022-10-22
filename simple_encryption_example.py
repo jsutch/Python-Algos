e1781b2c37f1e9dff9e4dc3b96c6f3d19fb361dc
Jeff Sutch <jeff@collettpark.com>
2022-10-22 15:07:17 -0700

    added simple stream encryption exercise

---
+++ simple_encryption_example.py
@@ -0,0 +1,45 @@
+"""
+A simple example of using XORs to do stream encryption
+
+"""
+# set some strings and keys
+In [36]: str1 = 'theres a lady whos sure all that glitters is gold'
+
+In [37]: str2 = 'a man a plan a canal panama'
+
+In [38]: key1 = 'abcdefg'
+
+# first example of key-length encryption
+In [42]: [ chr(ord(a) ^ ord(b)) for (a,b) in zip(str1, key1) ]
+Out[42]: ['\x15', '\n', '\x06', '\x16', '\x00', '\x15', 'G']
+
+In [43]: encrypted = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(str1, key1) ]
+
+In [44]: decrypted = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(encrypted, key1) ]
+
+In [45]: decrypted
+Out[45]: ['t', 'h', 'e', 'r', 'e', 's', ' ']
+
+
+# another example of key-length encryption
+In [46]: encrypted2 = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(str2, key1) ]
+
+In [47]: decrypted2 = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(encrypted2, key1) ]
+
+In [48]: decrypted2
+Out[48]: ['a', ' ', 'm', 'a', 'n', ' ', 'a']
+
+
+# adding itertools.cycle to apply multiple cycles of the keys for longer messages
+In [49]: from itertools import cycle
+
+In [51]: encrypted3 = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(str1, cycle(key1)) ]
+
+In [54]: encrypted3
+Out[54]: ['\x15', '\n', '\x06', '\x16', '\x00', '\x15', 'G', '\x00', 'B', '\x0f', '\x05', '\x01', '\x1f', 'G', '\x16', '\n', '\x0c', '\x17', 'E', '\x15', '\x12', '\x13', '\x07', 'C', '\x05', '\t', '\n', 'G', '\x15', '\n', '\x02', '\x10', 'E', '\x01', '\x0b', '\x08', '\x16', '\x17', '\x01', '\x17', '\x15', 'G', '\x08', '\x11', 'C', '\x03', '\n', '\n', '\x03']
+
+In [56]: decrypted3 = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(encrypted3, cycle(key1)) ]
+
+In [58]: "".join(decrypted3)
+Out[58]: 'theres a lady whos sure all that glitters is gold'
+
