aeb48b3f472b76e978beb0e74c8d83e3600c5314
Jeff Sutch <jeff@collettpark.com>
2022-11-07 11:35:37 -0800

    updated xor with example. added a simple main function

---
+++ simple_main.py
@@ -0,0 +1,22 @@
+In [12]: def main():
+    ...:     print('hello world')
+    ...:     for i in range(11):
+    ...:         print(i)
+    ...:
+
+In [13]: main
+Out[13]: <function __main__.main()>
+
+In [14]: main()
+hello world
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
--- simple_xor_encryption_example.py
+++ simple_xor_encryption_example.py
@@ -47,3 +47,109 @@ In [56]: decrypted3 = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(encrypted3, cycle(
 In [58]: "".join(decrypted3)
 Out[58]: 'theres a lady whos sure all that glitters is gold'
 
+
+
+"""
+helping to visualize the key byte and string byte streams
+"""
+# first with a string and a short key
+In [21]: key3='qwerty'
+
+In [22]: str3='hello there ladies and gentlemen'
+
+In [23]: [print(a, b) for (a,b) in zip(str3, cycle(key3)) ]
+h q
+e w
+l e
+l r
+o t
+  y
+t q
+h w
+e e
+r r
+e t
+  y
+l q
+a w
+d e
+i r
+e t
+s y
+  q
+a w
+n e
+d r
+  t
+g y
+e q
+n w
+t e
+l r
+e t
+m y
+e q
+n w
+
+In [23]: key4='homeboy throw in the towel'
+
+In [24]: str4='i wish i was a little bit taller i wish i was a baller'
+
+
+# then with two longer strings/keys
+In [25]: [print(a, b) for (a,b) in zip(str4, cycle(key4)) ]
+i h
+  o
+w m
+i e
+s b
+h o
+  y
+i
+  t
+w h
+a r
+s o
+  w
+a
+  i
+l n
+i
+t t
+t h
+l e
+e
+  t
+b o
+i w
+t e
+  l
+t h
+a o
+l m
+l e
+e b
+r o
+  y
+i
+  t
+w h
+i r
+s o
+h w
+
+i i
+  n
+w
+a t
+s h
+  e
+a
+  t
+b o
+a w
+l e
+l l
+e h
+r o
+
