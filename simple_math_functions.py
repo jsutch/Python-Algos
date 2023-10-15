b421b99bbcbe09b9bf5e7a1ba7d83731a5a7b9dd
Jeff Sutch <jeff@collettpark.com>
2023-10-14 18:17:58 -0700

    reverse update

--- simple_math_functions.py
+++ simple_math_functions.py
@@ -1,5 +1,14 @@
 def mycomma(num:int):
     """
-    return integer with commas demarcating the thousands
+    return integer with a string that has commas demarcating the thousands using f-strings
     """
     return f'{num:,}'
+
+
+
+def uncomma(num:str):
+    """
+    takes in a string of a number that has commas for separators and return the stripped token as an integer.
+    """
+    return int(num.replace(',',''))
+
